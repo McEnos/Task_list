5edcfa7ba83b5fe392344f43c8a025b20e1d2a28
ICTMSA <joshuamcenos@gmail.com>
2019-06-13 13:39:49 +0300

    Adding TaskFactory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,11 @@
+<?php 
+
+user Faker\Generator as Faker;
+use App\Models\Task;
+$factory->define(Task::class,function(Faker $faker){
+    return [
+        'title' => $faker->sentence,
+        'is_complete' => $faker->boolean,
+    ];
+});
+
