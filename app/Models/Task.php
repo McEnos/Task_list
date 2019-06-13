587ac75e637f5985326d70b7b94a5cd23819e139
ICTMSA <joshuamcenos@gmail.com>
2019-06-13 13:33:29 +0300

    Adding models relationship netween Task and User

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $casts = [
+        'is_complete' => 'boolean',
+    ];
+    protected $fillable = [
+        'title','is_complete',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
