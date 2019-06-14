398f48339e83760422069c618e64e65cfdac7c64
ICTMSA <joshuamcenos@gmail.com>
2019-06-14 09:29:30 +0300

    Adding views files

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use |App\Models\Task;
+use App\Models\Task;
 use App\Policies\TaskPolicy;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -16,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
-        Task::class => TaskPolicy::class;
+        Task::class => TaskPolicy::class,
     ];
 
     /**
