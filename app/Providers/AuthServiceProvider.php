fb55f31a11a7195dae1061a10306e69ceaf7c9c9
ICTMSA <joshuamcenos@gmail.com>
2019-06-14 08:58:27 +0300

    associating our policy with our Task model

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use |App\Models\Task;
+use App\Policies\TaskPolicy;
+// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
+        Task::class => TaskPolicy::class;
     ];
 
     /**
