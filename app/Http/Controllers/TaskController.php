8dec9d3354960b398c39bd926263f50e4fd9602f
ICTMSA <joshuamcenos@gmail.com>
2019-06-14 08:38:17 +0300

    Implementing methods of TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Auth::user()->tasks()->orderBy('is_completed')->orderByDesc('created_at')->paginate(5);
+        return view('tasks',compact('tasks'));
+    }
+    public function store(Request $request)
+    {
+        $data = $this->validate($request,[
+            'title' => 'required|string|max:255',
+        ]);
+        Auth::user()->tasks()->create([
+            'title' => $data['title'],
+            'is_completed' => false,
+        ]);
+        session()->flash('status','Task Created');
+        return redirect('/tasks');
+    }
 }
